4279109bdedbfa2e70cea8e34d7c58e8b64dc3ca
matejadb <92729211+matejadb@users.noreply.github.com>
2025-10-22 13:44:49 +0200

    figure out formula

--- scripts/app.js
+++ scripts/app.js
@@ -8,5 +8,18 @@ form.addEventListener('submit', (e) => {
 	const rateInput = document.getElementById('rate').value;
 	const typeInput = document.querySelector('input[name="type"]:checked').value;
 
+	let loanAmount = parseFloat(amountInput);
+	let interestRate = parseFloat(rateInput) / 12 / 100;
+	let loanTerm = parseFloat(termInput) * 12;
+
+	if (typeInput == 'repayment') {
+		let up = interestRate * Math.pow(1 + interestRate, loanTerm);
+		let down = Math.pow(1 + interestRate, loanTerm) - 1;
+		let monthlyPayment = loanAmount * (up / down);
+
+		console.log(Math.round(monthlyPayment * 100) / 100);
+		console.log(Math.round(monthlyPayment * loanTerm * 100) / 100);
+	}
+
 	console.log(amountInput, termInput, rateInput, typeInput);
 });
