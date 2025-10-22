061c73c0bd48a2dc88bf3d6743e12292922a471e
matejadb <92729211+matejadb@users.noreply.github.com>
2025-10-22 17:28:44 +0200

    add clear all functionality

--- scripts/app.js
+++ scripts/app.js
@@ -1,20 +1,22 @@
 const form = document.querySelector('.form-fields');
 const clearAll = document.querySelector('.clear-all');
-
+const amountInput = document.getElementById('amount');
+const termInput = document.getElementById('term');
+const rateInput = document.getElementById('rate');
 const emptyResult = document.querySelector('.results-container-empty');
 const completedResult = document.querySelector('.results-container-completed');
 
 form.addEventListener('submit', (e) => {
 	e.preventDefault();
 
-	const amountInput = document.getElementById('amount').value;
-	const termInput = document.getElementById('term').value;
-	const rateInput = document.getElementById('rate').value;
 	const typeInput = document.querySelector('input[name="type"]:checked').value;
 
-	let loanAmount = parseFloat(amountInput);
-	let interestRate = parseFloat(rateInput) / 12 / 100;
-	let loanTerm = parseFloat(termInput) * 12;
+	/**
+	 * If a user enters 300,000 it will remove the comma and just parse 300000
+	 */
+	let loanAmount = parseFloat(amountInput.value.replace(/,/g, ''));
+	let interestRate = parseFloat(rateInput.value) / 12 / 100;
+	let loanTerm = parseFloat(termInput.value) * 12;
 
 	if (typeInput == 'repayment') {
 		let up = interestRate * Math.pow(1 + interestRate, loanTerm);
@@ -40,6 +42,13 @@ form.addEventListener('submit', (e) => {
 
 clearAll.addEventListener('click', (e) => {
 	e.preventDefault();
+
+	amountInput.value = '';
+	termInput.value = '';
+	rateInput.value = '';
+
+	const checkedType = document.querySelector('input[name="type"]:checked');
+	if (checkedType) checkedType.checked = false;
 
 	emptyResult.classList.remove('hidden');
 	completedResult.classList.add('hidden');
