9cca1061db843fed424b014eb2a985d876a4a55d
matejadb <92729211+matejadb@users.noreply.github.com>
2025-10-23 14:40:12 +0200

    code refactor

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,5 @@
+// DECLARATIONS
+
 const form = document.querySelector('.form-fields');
 const clearAll = document.querySelector('.clear-all');
 const amountInput = document.getElementById('amount');
@@ -5,6 +7,8 @@ const termInput = document.getElementById('term');
 const rateInput = document.getElementById('rate');
 const emptyResult = document.querySelector('.results-container-empty');
 const completedResult = document.querySelector('.results-container-completed');
+const inputContainers = document.querySelectorAll('.input-container');
+const options = document.querySelectorAll('.option');
 const amountError = amountInput
 	.closest('.mortgage-amount')
 	.querySelector('.error-text');
@@ -23,6 +27,8 @@ const suffixRate = rateInput
 	.closest('.input-container')
 	.querySelector('.suffix-text');
 
+// MAIN
+
 form.addEventListener('submit', (e) => {
 	e.preventDefault();
 
@@ -37,59 +43,103 @@ form.addEventListener('submit', (e) => {
 
 	if (!isValid) return;
 
-	function calculatePayment(){
-		
-	}
-
-	/**
-	 * If a user enters 300,000 it will remove the comma and just parse 300000
-	 */
-	let loanAmount = parseFloat(amountInput.value.replace(/,/g, ''));
-	let interestRate = parseFloat(rateInput.value) / 12 / 100;
-	let loanTerm = parseFloat(termInput.value) * 12;
-	const monthly = document.querySelector('.monthly-payment');
-	const total = document.querySelector('.total-payment');
-	if (typeInput.value == 'repayment') {
-		let up = interestRate * Math.pow(1 + interestRate, loanTerm);
-		let down = Math.pow(1 + interestRate, loanTerm) - 1;
-		let monthlyPayment = loanAmount * (up / down);
-
-		monthly.textContent =
-			'£' + (Math.round(monthlyPayment * 100) / 100).toLocaleString('en-US');
-
-		total.textContent =
-			'£' +
-			(Math.round(monthlyPayment * loanTerm * 100) / 100).toLocaleString(
-				'en-US'
-			);
-	} else if (typeInput.value == 'interest-only') {
-		let monthlyPayment = loanAmount * interestRate;
-		monthly.textContent =
-			'£' + (Math.round(monthlyPayment * 100) / 100).toLocaleString('en-US');
-
-		total.textContent =
-			'£' +
-			(Math.round(monthlyPayment * loanTerm * 100) / 100).toLocaleString(
-				'en-US'
-			);
-	}
+	calculatePayments(
+		amountInput.value,
+		rateInput.value,
+		termInput.value,
+		typeInput.value
+	);
 
 	emptyResult.classList.add('hidden');
 	completedResult.classList.remove('hidden');
 });
 
-function validateInput(amount, term, rate, type) {
-	let isValid = true;
+document.addEventListener('click', () => {
+	removeInputContainerFocus();
+});
 
+inputContainers.forEach((container) => {
+	container.addEventListener('click', (e) => {
+		e.stopPropagation();
+
+		const input = container.querySelector('input');
+		input.focus();
+
+		removeInputContainerFocus();
+
+		const focusEl = input.closest('.input-container');
+		const focusElSpan = focusEl.querySelector('span');
+
+		focusEl.classList.add('input-container-focus');
+		focusElSpan.classList.add('input-container-span-focus');
+	});
+});
+
+options.forEach((option) => {
+	option.addEventListener('click', () => {
+		const radio = option.querySelector('input[name="type"]');
+		if (radio) {
+			removeRadioChecked();
+
+			radio.checked = true;
+			option.classList.add('radio-checked');
+		}
+	});
+});
+
+// FUNCTIONS
+
+function addErrorHiddenClass() {
 	amountError.classList.add('hidden');
 	termError.classList.add('hidden');
 	rateError.classList.add('hidden');
 	typeError.classList.add('hidden');
+}
 
+function removeRadioChecked() {
+	options.forEach((opt) => opt.classList.remove('radio-checked'));
+}
+
+function removeInputContainerFocus() {
+	inputContainers.forEach((inp) => {
+		inp.classList.remove('input-container-focus');
+		const focusSpan = inp.querySelector('span');
+
+		focusSpan.classList.remove('input-container-span-focus');
+	});
+}
+
+function removeTextErrorClass() {
 	prefix.classList.remove('prefix-text-error');
 	suffixTerm.classList.remove('prefix-text-error');
 	suffixRate.classList.remove('prefix-text-error');
+}
 
+clearAll.addEventListener('click', (e) => {
+	e.preventDefault();
+
+	amountInput.value = '';
+	termInput.value = '';
+	rateInput.value = '';
+
+	const checkedType = document.querySelector('input[name="type"]:checked');
+	if (checkedType) checkedType.checked = false;
+
+	removeInputContainerFocus();
+	removeRadioChecked();
+
+	emptyResult.classList.remove('hidden');
+	completedResult.classList.add('hidden');
+
+	removeTextErrorClass();
+	addErrorHiddenClass();
+});
+
+function validateInput(amount, term, rate, type) {
+	let isValid = true;
+
+	addErrorHiddenClass();
+	removeTextErrorClass();
 	if (!amount) {
 		amountError.classList.remove('hidden');
 		prefix.classList.add('prefix-text-error');
@@ -113,78 +163,41 @@ function validateInput(amount, term, rate, type) {
 	return isValid;
 }
 
-clearAll.addEventListener('click', (e) => {
-	e.preventDefault();
+function calculatePayments(amount, rate, term, type) {
+	const monthly = document.querySelector('.monthly-payment');
+	const total = document.querySelector('.total-payment');
 
-	amountInput.value = '';
-	termInput.value = '';
-	rateInput.value = '';
+	let loanAmount = parseFloat(amount.replace(/,/g, ''));
+	let interestRate = parseFloat(rate) / 12 / 100;
+	let loanTerm = parseFloat(term) * 12;
 
-	const checkedType = document.querySelector('input[name="type"]:checked');
-	if (checkedType) checkedType.checked = false;
+	switch (type) {
+		case 'repayment':
+			let up = interestRate * Math.pow(1 + interestRate, loanTerm);
+			let down = Math.pow(1 + interestRate, loanTerm) - 1;
+			monthlyPayment = loanAmount * (up / down);
 
-	inputContainers.forEach((inp) => {
-		inp.classList.remove('input-container-focus');
-		const focusSpan = inp.querySelector('span');
+			monthly.textContent =
+				'£' + (Math.round(monthlyPayment * 100) / 100).toLocaleString('en-US');
 
-		focusSpan.classList.remove('input-container-span-focus');
-	});
-	options.forEach((opt) => opt.classList.remove('radio-checked'));
+			total.textContent =
+				'£' +
+				(Math.round(monthlyPayment * loanTerm * 100) / 100).toLocaleString(
+					'en-US'
+				);
+			break;
+		case 'interest-only':
+			monthlyPayment = loanAmount * interestRate;
+			monthly.textContent =
+				'£' + (Math.round(monthlyPayment * 100) / 100).toLocaleString('en-US');
 
-	emptyResult.classList.remove('hidden');
-	completedResult.classList.add('hidden');
-	prefix.classList.remove('prefix-text-error');
-	suffixTerm.classList.remove('prefix-text-error');
-	suffixRate.classList.remove('prefix-text-error');
-	amountError.classList.add('hidden');
-	termError.classList.add('hidden');
-	rateError.classList.add('hidden');
-	typeError.classList.add('hidden');
-});
-
-const inputContainers = document.querySelectorAll('.input-container');
-
-inputContainers.forEach((container) => {
-	container.addEventListener('click', (e) => {
-		e.stopPropagation();
-
-		const input = container.querySelector('input');
-		input.focus();
-
-		inputContainers.forEach((inp) => {
-			inp.classList.remove('input-container-focus');
-			const focusSpan = inp.querySelector('span');
-
-			focusSpan.classList.remove('input-container-span-focus');
-		});
-
-		const focusEl = input.closest('.input-container');
-		const focusElSpan = focusEl.querySelector('span');
-
-		focusEl.classList.add('input-container-focus');
-		focusElSpan.classList.add('input-container-span-focus');
-	});
-});
-
-const options = document.querySelectorAll('.option');
-
-options.forEach((option) => {
-	option.addEventListener('click', () => {
-		const radio = option.querySelector('input[name="type"]');
-		if (radio) {
-			options.forEach((opt) => opt.classList.remove('radio-checked'));
-
-			radio.checked = true;
-			option.classList.add('radio-checked');
-		}
-	});
-});
-
-document.addEventListener('click', () => {
-	inputContainers.forEach((inp) => {
-		inp.classList.remove('input-container-focus');
-		const focusSpan = inp.querySelector('span');
-
-		focusSpan.classList.remove('input-container-span-focus');
-	});
-});
+			total.textContent =
+				'£' +
+				(Math.round(monthlyPayment * loanTerm * 100) / 100).toLocaleString(
+					'en-US'
+				);
+			break;
+		default:
+			console.error('There has been an error');
+	}
+}
